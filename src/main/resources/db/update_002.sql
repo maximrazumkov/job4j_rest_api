7781f371b0cde819a193a3faab89109baf5c9133
Maxim Razumkov <m.razumkov@vniims.ru>
2021-03-04 11:21:59 +0300

    Added password for admin

--- src/main/resources/db/update_002.sql
+++ src/main/resources/db/update_002.sql
@@ -1,2 +1,4 @@
 ALTER TABLE person RENAME COLUMN name to username;
 ALTER TABLE person ADD COLUMN password text;
+
+update public.person set password = '$2a$10$CQADw1DjFbUMsQ8oBoP7ju3SuacVTkRq6YRQjDN5QYQcHPeJEFLNO' where id = 3
